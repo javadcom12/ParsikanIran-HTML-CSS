0b8526538ee4c944abb0bdfbe150732557b3d8e9
javadcom12 <sharifi.com@engineer.com>
2023-11-24 18:06:25 +0330

    ParsikanIran practice HTML & CSS

---
+++ js/pars.js
@@ -0,0 +1,33 @@
+function fo() {      
+    document.getElementById("im").src="./img/icon/1.png" 
+}
+function pd() {
+document.getElementById("im").src="./img/icon/11.png"
+}
+
+
+function fo1() {      
+    document.getElementById("im1").src="./img/icon/2.png" 
+}
+function pd1() {
+document.getElementById("im1").src="./img/icon/22.png"
+}
+
+
+
+function fo2() {      
+    document.getElementById("im2").src="./img/icon/3.png" 
+}
+function pd2() {
+document.getElementById("im2").src="./img/icon/33.png"
+}
+
+
+
+function fo3() {      
+    document.getElementById("im3").src="./img/icon/4.png" 
+}
+function pd3() {
+document.getElementById("im3").src="./img/icon/44.png"
+}
+
